1e842b058009ba3225e50f9d0b4526d4be778832
hallumse <hetoria@yahoo.com>
2019-02-12 14:18:02 -0500

    deleting app public spec files out of folder

--- app/models/author.rb
+++
@@ -1,4 +0,0 @@
-class Author < ApplicationRecord
-  has_many :author_books
-  has_many :books, through: :author_books
-end
--- app/services/initializers/stripe.rb
+++
@@ -1,2 +0,0 @@
-require "stripe"
-Stripe.api_key = Etoria_Ebooks_2::Application.credentials.stripe[:secret_key]
